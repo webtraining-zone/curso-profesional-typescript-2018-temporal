4046a9c39636aa48bf37499201270ac0aa35edaf
Alex Arriaga <alexzander.arriaga@base22.com>
2018-06-26 19:29:21 -0500

    Agregar ejemplo de app

---
+++ src/09-complex-application/app/index.ts
@@ -0,0 +1,7 @@
+class App {
+    start() {
+        console.log('>> Start()!');
+    }
+}
+
+new App().start();
--- src/09-complex-application/webpack.config.js
+++ src/09-complex-application/webpack.config.js
@@ -1,33 +1,35 @@
 module.exports = {
-    entry: "./app.ts",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+  entry: './app/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  watch: true,
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            {test: /\.tsx?$/, loader: "awesome-typescript-loader"},
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
-        ]
-    },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {},
-    mode: 'production'
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {enforce: 'pre', test: /\.js$/, loader: 'source-map-loader'}
+    ]
+  },
 
-};
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {},
+  mode: 'production'
+
+}
